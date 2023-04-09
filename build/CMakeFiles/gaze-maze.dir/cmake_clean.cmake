cc56732803738d68e893f4ad955584420875aa15
Back777space <ivan.molenaers@hotmail.com>
2023-04-09 20:33:34 +0200

    init

---
+++ build/CMakeFiles/gaze-maze.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gaze-maze.dir/main.cpp.o"
+  "CMakeFiles/gaze-maze.dir/main.cpp.o.d"
+  "CMakeFiles/gaze-maze.dir/stb_image.cpp.o"
+  "CMakeFiles/gaze-maze.dir/stb_image.cpp.o.d"
+  "gaze-maze"
+  "gaze-maze.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gaze-maze.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/gaze-maze.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for gaze-maze.
+# This may be replaced when dependencies are built.
